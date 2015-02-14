913f932c86356a0c3ecfaa06ca0c18fd07fe4fb8
mobjohnson <mobjohnson@gmail.com>
2015-02-14 12:22:37 -0500

    Remove senator collection

--- app/assets/javascripts/collections/senatorCollection.js
+++
@@ -1,7 +0,0 @@
-App.SenatorCollection = Backbone.Collection.extend({
-  model: App.SenatorModel,
-  url: '/senators',
-  initialize: function(){
-    console.log('New Senator Collection');
-  }
-});
